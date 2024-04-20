b71cf880de230776c5b9be92f40ab67d15f25560
Abhishek Ghosh <aghosh0188@gmail.com>
2024-04-20 14:11:39 +0530

    update widget_test configuration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,18 +15,16 @@ void main() {
     await tester.pumpWidget(const LoginPage());
 
     // Verify that our counter starts at 0.
-    expect(find.text('Save'), findsOneWidget);
-    expect(find.text('1'),
-        findsNothing); // Expecting '1' to not be found initially
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'),
-        findsNothing); // Expecting '0' to not be found after tapping
-    expect(find.text('1'),
-        findsOneWidget); // Expecting '1' to be found after tapping
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
