e3189cf8c9dad5044bf5a0d5f6b51d320e1f96e8
Abhishek Ghosh <aghosh0188@gmail.com>
2024-04-20 14:24:31 +0530

    update widget_test configuration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,12 +11,7 @@ import 'package:login_drawer_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(
-      MaterialApp(
-        // or WidgetsApp if appropriate
-        home: LoginPage(),
-      ),
-    );
+    await tester.pumpWidget(const LoginPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
